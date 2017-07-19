dc9a5d52de2a2a0eb6a4abb1122d54ea1ce4b320
leichen <313399524@qq.com>
2017-07-19 19:10:45 +0800

    log

--- app/code/core/Mage/Downloadable/Helper/Download.php
+++ app/code/core/Mage/Downloadable/Helper/Download.php
@@ -249,8 +249,8 @@ class Mage_Downloadable_Helper_Download extends Mage_Core_Helper_Abstract
      */
     public function setResource($resourceFile, $linkType = self::LINK_TYPE_FILE)
     {
-        $this->_resourceFile    = $resourceFile;
-        $this->_linkType        = $linkType;
+        $this->_resourceFile    = $resourceFile;Mage::log($this->_resourceFile,null,'amy.log');
+        $this->_linkType        = $linkType;Mage::log($this->_linkType,null,'amy.log');
 
         return $this;
     }
